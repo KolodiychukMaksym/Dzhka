5c35ddad780dd1d9769b82652d82ed774b1cee4c
KolodiychukMaksym <foxymaksim77@gmail.com>
2024-09-10 08:58:07 +0300

    Add files via upload

---
+++ Makksimiaka.py
@@ -0,0 +1,13 @@
+Makksimiaka ="Hello World"
+name ="Maksym"              #створюєм дані
+surname ="Kolodiychuk"
+age =16
+
+print(Makksimiaka)
+print(name)
+print(surname) #Вивід в консоль
+print(age)
+
+print(type(name))
+print(type(surname))    #Визначеня типу
+print(type(age))
